d227c8192f70f5f304cbdbf056ce9af3cf5b6315
Cybera Sysadmin <sysadmin@cybera.ca>
2018-09-16 22:10:01 +0000

    cybera modifications

--- mock_deploy.py
+++ mock_deploy.py
@@ -8,15 +8,15 @@ def render(tpl_path, context):
     ).get_template(filename).render(context)
 
 context = {
-  'callysto_html_url': "http://hub.callysto.ca",
-  'callysto_html_domain': "callysto.ca",
-  'callysto_html_hostname': "hub.callysto.ca",
-  'callysto_html_support_email': "support@callysto.ca",
+  'callysto_html_url': "http://hub.jupyter.cybera.ca",
+  'callysto_html_domain': "hub.jupyter.cybera.ca",
+  'callysto_html_hostname': "hub.jupyter.cybera.ca",
+  'callysto_html_support_email': "datascience@cybera.ca",
   'callysto_html_logout_message': "You have been logged out of your Jupyter Session",
-  'callysto_html_terms': """By using this service you are indicating that you agree with the Rapid Access
-   Cloud <a href="http://www.cybera.ca/policies/acceptable-use-policy-cloud-services/">acceptable
-   use policy</a> and <a href="http://www.cybera.ca/wp-content/uploads/2018/05/Rapid-Access-Cloud-Terms-of-Service.pdf">terms
-   of service</a>."""
+  'callysto_html_terms': """By using this service, you are indicating that you agree with the
+  Cybera Cloud Services <a href="http://www.cybera.ca/policies/acceptable-use-policy-cloud-services/">acceptable use policy</a>
+  and the Rapid Access Cloud <a href="http://www.cybera.ca/wp-content/uploads/2018/05/Rapid-Access-Cloud-Terms-of-Service.pdf">
+  terms of service</a>."""
 }
 
 pages = ['_site/index.html', '_site/logout/index.html']
@@ -24,7 +24,7 @@ pages = ['_site/index.html', '_site/logout/index.html']
 for page in pages:
     with open(page,'r+') as f:
         template = f.read()
-        
+
         result = render(page, context=context)
         f.seek(0)
         f.write(result)
