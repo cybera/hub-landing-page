4edf3ace15c692a46052fd1c68b009d380c8f350
Cybera Sysadmin <sysadmin@cybera.ca>
2019-03-19 19:37:08 +0000

    Support for multiple branches

--- ansible.sh
+++ ansible.sh
@@ -4,6 +4,11 @@ DIR=$(dirname "$0")
 
 cd $DIR
 
+branch="template-callysto"
+if [[ -n $1 ]]; then
+  branch="template-${1}"
+fi
+
 if [[ $(git status -s) ]]
 then
     echo "The working directory is dirty. Please commit any pending changes."
@@ -16,8 +21,8 @@ mkdir _site
 git worktree prune
 rm -rf .git/worktrees/_site
 
-echo "Checking out template-callysto branch into public"
-git worktree add -B template-callysto _site origin/template-callysto
+echo "Checking out $branch branch into public"
+git worktree add -B $branch _site origin/$branch
 
 echo "Removing existing files"
 rm -rf _site/*
@@ -25,5 +30,5 @@ rm -rf _site/*
 echo "Generating site"
 jekyll build
 
-echo "Updating template-callysto branch"
+echo "Updating $1 branch"
 cd _site && git add --all && git commit -m "Publishing to template (ansible.sh)" && git push
